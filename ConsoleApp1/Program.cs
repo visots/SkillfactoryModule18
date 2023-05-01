ffce9946352b327345855ccf9830b481a34b393e
visot <visotskiy.alexey@gmail.com>
2023-05-01 16:06:39 +0700

    Модуль 18 Паттерны

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsoleApp1
+﻿namespace Task18_3_8
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // запрос выгрузки информации с сайта
+            string uri = "http://192.250.225.62/";
+            string resource = "?965502";
+
+            await LoadSite(uri, resource);
+        }
+
+        static async Task LoadSite(string uri, string resource)
+        {
+            // создаем клиента для http-запросов
+            var httpClient = SiteLoader.GetHttpClient(uri);
+
+            // задаем адрес сайта
+            httpClient.BaseAddress = new Uri(uri);
+
+            // запрашиваем нужный ресурс
+            var result = await httpClient.GetAsync(resource);
+
+            // считываем ответ в строку и выводим
+            var contentString = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(contentString);
         }
     }
 }
---
+++ ConsoleApp1/SiteLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task18_3_8
+{
+    internal class SiteLoader
+    {
+        private static HttpClient httpClient;
+
+        public string siteUri { get; private set; }
+
+        protected SiteLoader(string resourceUrl)
+        {
+            siteUri = resourceUrl;
+        }
+
+        public static HttpClient GetHttpClient(string siteUri) 
+        { 
+            if (httpClient == null)
+            {
+                httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(siteUri);
+            }
+            return httpClient;
+        }
+
+    }
+}
